5a2c7e0a235b928fc37a302c105d65e8df1cbd0a
Cobrettie <cobrettiegarner3@gmail.com>
2020-07-22 17:27:34 -0400

    configure login functionality

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const authModel = require('./auth-model.js');
+require("dotenv").config(); // for reading JWT_SECRET from .env file
 
 function logError(err) {
   console.log('Error: ', err)
@@ -41,5 +42,45 @@ router.post('/register', async (req, res) => {
     logError(err)
   }
 })
+
+router.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await authModel.findBy({ username }).first()
+    if (!user) {
+      res.status(401).json({
+        message: 'Invalid credentials'
+      })
+    }
+
+    const validPassword = await bcrypt.compare(password, user.password)
+    if (!validPassword) {
+      res.status(401).json({
+        message: 'Invalid credentials'
+      })
+    }
+
+    const token = generateToken(user)
+    res.cookie('token', token)
+    res.json({
+      message: `Hello ${username}`,
+      token: token  // token for client, i do believe? ask  
+    })
+
+
+  } catch(err) {
+    logError(err)
+  }
+})
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    usertype: user.user_type
+  }
+
+  return jwt.sign(payload, process.env.JWT_SECRET)
+}
 
 module.exports = router;
