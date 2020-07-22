98ff2ccd64d842116cd6087304a7d5c7fd1190c1
Cobrettie <cobrettiegarner3@gmail.com>
2020-07-22 15:53:36 -0400

    configure users migration table

---
+++ database/migrations/20200722154634_users.js
@@ -0,0 +1,15 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('users', table => {
+    table.increment(); // 'id', primary key, auto incrementing
+    table
+      .string('username', 255)
+      .notNullable()
+      .unique()
+    table.string('password', 255).notNullable()
+    table.string('user_type').notNullable()
+  })  
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('users')
+};
