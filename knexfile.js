817b83a645f97a099e277b720c2d8bdab6e8b973
Cobrettie <cobrettiegarner3@gmail.com>
2020-07-24 20:23:29 -0400

    configure postgres for production environment

--- knexfile.js
+++ knexfile.js
@@ -12,16 +12,26 @@ module.exports = {
     seeds: { directory: './database/seeds' }
   },
   testing: {
-    client: "sqlite3",
+    client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: "./data/testing.db3",
+      filename: './data/testing.db3',
     },
     migrations: {
-      directory: "./data/migrations",
+      directory: './data/migrations',
     },
     seeds: {
-      directory: "./data/seeds",
+      directory: './data/seeds',
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
     }
   }
 };
