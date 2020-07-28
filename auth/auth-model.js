da0457e6477f3bbf0f50b11d232886633f690951
Cobrettie <cobrettiegarner3@gmail.com>
2020-07-27 20:33:42 -0400

    rollback, latest

--- auth/auth-model.js
+++ auth/auth-model.js
@@ -4,9 +4,8 @@ function getAllUsers() {
   return db('users')
 }
 
-async function addUser(user) {
-  const [id] = await db('users').insert(user, 'id');
-  return db('users').where({ id }).first();
+function addUser(user) {
+  return db('users').insert(user, 'id');
 }
 
 function findBy(filter) {
