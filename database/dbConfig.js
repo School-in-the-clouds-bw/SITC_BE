c201a85be4e2316a4327b1512980d09b39888cda
Cobrettie <cobrettiegarner3@gmail.com>
2020-07-27 21:29:12 -0400

    change to sqlite3

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -2,5 +2,6 @@ const knex = require("knex")
 const knexfile = require("../knexfile.js")
 
 const dbEnv = 'development';
+// process.env.DB_ENV <-- from postgres config vars
 
 module.exports = knex(knexfile[dbEnv]);
